d875c8c96ac575a450efe3e094af8347f32db5fb
Bharath Krishna Vishnuram <bharath484@gmail.com>
2022-02-09 01:07:03 +0530

    Added problems based on Carry forward logic. And problems related to Subarrays.

--- CodingProblems.WebApi/Controllers/CarryForwardController.cs
+++ CodingProblems.WebApi/Controllers/CarryForwardController.cs
@@ -10,11 +10,10 @@ namespace CodingProblems.WebApi.Controllers
     public class CarryForwardController : CodingControllerBase
     {
         /// <summary>
-        /// You have given a string A having Uppercase English letters.
+        /// In a string A having Uppercase letters.
         /// You have to find that how many times subsequence "AG" is there in the given string.
-        /// NOTE: Return the answer modulo 109 + 7 as the answer can be very large
         /// </summary>
-        /// <returns>Return an integer denoting the number of subsequences.</returns>   
+        /// <returns>Return an integer denoting the number of subsequences modulo 109 + 7.</returns>   
         [HttpPost]
         public int SubsequencesAG(String A)
         {
@@ -28,15 +27,16 @@ namespace CodingProblems.WebApi.Controllers
                 if (A[i] == 'G')
                     count += t;
             }
-            return count%mod;
+            return count % mod;
         }
 
         /// <summary>
-        /// Given an array A. Find the size of the smallest subarray such that 
-        /// it contains atleast one occurrence of the maximum value of the array
-        /// and atleast one occurrence of the minimum value of the array.
+        /// Given an array A. Find the size of the smallest subarray with
+        /// atleast one occurrence of the maximum value 
+        /// and the minimum value of the array.
         /// </summary>
-        /// <returns>Return length of the smallest subarray which has atleast one occurrence of minimum and maximum element of the array.</returns>   
+        /// <returns>Return length of the smallest subarray which has 
+        /// atleast one occurrence of minimum and maximum element of the array.</returns>   
         [HttpPost]
         public int ClosestMinMax(int[] A)
         {
@@ -67,6 +67,75 @@ namespace CodingProblems.WebApi.Controllers
                 minLen = Math.Min(minLen, temp);
             }
             return minLen;
+        }
+
+        /// <summary>
+        /// Given an initial state of all bulbs, 
+        /// find the minimum number of switches you have to press to turn on all the bulbs
+        /// </summary>
+        /// <returns>integer representing the minimum number of switches required.</returns>   
+        [HttpPost]
+        public int SwitchBulbs(int[] A)
+        {
+            int toggle = 0;
+            foreach(var a in A)
+                toggle += 1 - (a ^ (toggle & 1));
+            return toggle;
+        }
+
+        /// <summary>
+        /// find all the amazing substrings of S.
+        /// Amazing Substring is one that starts with a vowel(a, e, i, o, u, A, E, I, O, U).
+        /// </summary>
+        /// <returns>integer X mod 10003, here X is number of Amazing Substrings in given string.</returns>   
+        [HttpPost]
+        public int AmazingSubarrays(String A)
+        {
+            int cnt = 0;
+            String vowels = "aeiouAEIOU";
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (vowels.IndexOf(A[i]) != -1)
+                    cnt += A.Length - i;
+            }
+            return cnt% 10003;
+        }
+
+        /// <summary>
+        /// In integer array A, decide whether it is possible to divide the array into 
+        /// one or more subarrays of even length such that first and last element of all subarrays will be even.
+        /// </summary>
+        /// <returns>Return "YES" if it is possible otherwise return "NO" (without quotes).</returns>   
+        [HttpPost]
+        public String EvenSubArrays(int[] A)
+        {
+            int n = A.Length;
+            if (n % 2 != 0 || (A[0] % 2 != 0) || A[n - 1] % 2 != 0)
+                return "NO";
+            return "YES";
+        }
+
+        /// <summary>
+        /// In integer array A,  find all the leaders in the array A.
+        /// An element is leader if it is strictly greater than all the elements to its right side.
+        /// </summary>
+        /// <returns>integer array denoting all the leader elements of the array.</returns>   
+        [HttpPost]
+        public int[] LeadersInArray(int[] A)
+        {
+            int n = A.Length;
+            List<int> t = new List<int>();
+            int leader = A[n - 1];
+            t.Add(leader);
+            for (int j = n - 2; j >= 0; j--)
+            {
+                if (A[j] > leader)
+                {
+                    leader = A[j];
+                    t.Add(leader);
+                }
+            }
+            return t.ToArray();
         }
     }
 }
---
+++ CodingProblems.WebApi/Controllers/SubArraysController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingProblems.WebApi.Controllers
+{
+    public class SubArraysController : CodingControllerBase
+    {
+
+        /// <summary>
+        /// Find the contiguous non empty subarray within an array, A of length N which has the largest sum
+        /// </summary>
+        /// <returns>Integer representing the maximum possible sum of the contiguous subarray.</returns>   
+        [HttpPost]
+        public int MaxSubArray(int[] A)
+        {
+            int maxSum = A[0];
+            int currSum = A[0];
+            for (int i = 1; i < A.Length; i++)
+            {
+                currSum = Math.Max(currSum + A[i], A[i]);
+                maxSum = Math.Max(maxSum, currSum);
+            }
+            return maxSum;
+        }
+
+        /// <summary>
+        /// In an integer array A find the sum of all subarray sums of A
+        /// </summary>
+        /// <returns>integer denoting the sum of all subarray sums of the given array.</returns>   
+        [HttpPost]
+        public long SubarraySum(int[] A)
+        {
+            int n = A.Length;
+            long overallSum = 0;
+            for (int index = 0; index < n; index++)
+                overallSum += ((index + 1l) * (n - index) * A[index]);
+            return overallSum;
+        }
+
+        /// <summary>
+        /// In an integer array A, Find the subarray with least average of size k
+        /// </summary>
+        /// <returns>index of the first element of the subarray of size k that has least average.</returns>   
+        [HttpPost]
+        public int LeastSubarrayAverage(int[] A, int K)
+        {
+            int sum = 0;
+            int n = A.Length;
+            for (int i = 0; i < K; i++)
+                sum += A[i];
+            int index = 0;
+            int prevSum = sum;
+            for (int i = 0; i < n - K; i++)
+            {
+                sum += A[i + K] - A[i];
+                if (sum < prevSum)
+                {
+                    index = i + 1;
+                    prevSum = sum;
+                }
+                // System.out.println(prevSum + " " + sum + " " + A[i] + " " + A[i+B] + " " + index);
+            }
+            return index;
+        }
+
+        /// <summary>
+        /// In integer array C. Find a subarray (contiguous elements) so that 
+        /// the sum of contiguous elements is maximum.
+        /// But the sum must not exceed B
+        /// </summary>
+        /// <returns>Returns count of good subarrays in A.</returns>   
+        [HttpPost]
+        public int MaxSubarray(int B, int[] C)
+        {
+
+            int curr = 0, prev = 0, max = 0, sum = 0;
+            while (curr < C.Length)
+            {
+                sum += C[curr];
+                if (sum <= B && sum > max)
+                    max = sum;
+                if (sum > B)
+                {
+                    while (prev <= curr && sum > B)
+                    {
+                        sum -= C[prev];
+                        prev++;
+                    }
+                    if (sum > max) 
+                        max = sum;
+                }
+                curr++;
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// In array A of 0 and 1's, and an integer B.
+        /// Find all the indices as centre of 2 * B + 1 length 0-1 of alternating subarray.
+        /// Alternating array contains only 0's and 1's, and having no adjacent 0's or 1's.
+        /// </summary>
+        /// <returns>Return an integer array containing indices(0-based) in sorted order. 
+        /// If no such index exists, return an empty integer array.</returns>   
+        /// For e.g.arrays[0, 1, 0, 1], [1, 0] and[1] are 0-1 alternating, while [1, 1] and[0, 1, 0, 0, 1] are not.
+        [HttpPost]
+        public List<int> AlternatingSubarray(List<int> A, int B)
+        {
+            List<int> C = new List<int>();
+            int n = A.Count(), len = 2 * B + 1, prev = A[0], count = 1;
+            bool isValid = false;
+            for (int i = 0; i < n; i++)
+            {
+                // Console.WriteLine(prev + " " + A[i] + " " + count +  " " + i + " " + C.Count() + " " + isValid);
+                isValid = prev != A[i];
+                if (!isValid)
+                    count = 1;
+                else if (count != len)
+                    count++;
+                if (count == len)
+                {
+                    C.Add(i - B);
+                    count--;
+                }
+                prev = A[i];
+            }
+            return C;
+        }
+    }
+}
